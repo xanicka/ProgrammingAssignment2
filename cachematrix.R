02bf1e6baf00944f99b80e5ee103c0b7a44c2e9b
xanicka <hunchback@seznam.cz>
2014-06-20 21:22:06 +0200

    Code for makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            inv <- NULL
+            set <- function(y) {
+                        x <<- y
+                        inv <<- NULL
+            }
+            
+            get<-function() x
+            
+            setInversion <- function(solve) inv <<- solve
+            
+            getInversion <- function() inv
+            
+            list(set = set, 
+                 get = get, 
+                 setInversion = setInversion, 
+                 getInversion = getInversion)
 }
 
 
